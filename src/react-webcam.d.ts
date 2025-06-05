859f90f92b7ae00c9a6a87554efd5f6cf89906b5
Satyam <sc10247@nyu.edu>
2025-06-04 23:58:08 -0400

    feat: Add react app alongwith pwa

---
+++ src/react-webcam.d.ts
@@ -0,0 +1,18 @@
+declare module 'react-webcam' {
+  import React from 'react';
+
+  interface WebcamProps {
+    audio?: boolean;
+    height?: number;
+    width?: number;
+    screenshotFormat?: 'image/jpeg' | 'image/png' | 'image/webp';
+    style?: React.CSSProperties;
+    videoConstraints?: MediaStreamConstraints;
+    onUserMedia?: (stream: MediaStream) => void;
+    onUserMediaError?: (error: string) => void;
+  }
+
+  export default class Webcam extends React.Component<WebcamProps> {
+    getScreenshot: () => string | null;
+  }
+} 
