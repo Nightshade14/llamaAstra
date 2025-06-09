206cb0075636068c33af5fadb641d2be3e070491
Satyam Chatrola <satyamchatrola14@gmail.com>
2025-06-09 00:14:45 -0400

    feat: Add webcam feed to the web-page

--- src/react-webcam.d.ts
+++ src/react-webcam.d.ts
@@ -7,6 +7,7 @@ declare module 'react-webcam' {
     width?: number;
     screenshotFormat?: 'image/jpeg' | 'image/png' | 'image/webp';
     style?: React.CSSProperties;
+    className?: string;
     videoConstraints?: MediaStreamConstraints;
     onUserMedia?: (stream: MediaStream) => void;
     onUserMediaError?: (error: string) => void;
